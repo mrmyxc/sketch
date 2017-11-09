7aba4a0e4f53a66afff9aaa57b365ad39728334f
Gbenga Abolumo <gbenga.ab@gmail.com>
2017-11-09 08:45:56 +0000

    added colour options

--- jsscript.js
+++ jsscript.js
@@ -4,20 +4,35 @@ $(document).ready(() => {
     const $gridContainer = $(".grids");
     var gridWidth;
     var cellDimension;
-    ClearAndPrompt();
+    var max = 1;
+    var opacity = 0;
 
+    $("#solidButton").click(function () {
+        onHoverNormal();
+    });
 
-    $("#clearButton").click(function () {
-        ClearAndPrompt();
+    $("#trailButton").click(function () {
+        onHoverTrail();
+    });
+
+    $("#opacityButton").click(function () {
+        onHoverOpacity();
+    });
+
+    $("#randomButton").click(function () {
+        onHoverRandom();
     });
 
     $("#refreshButton").click(function () {
-        $(".cells").css("background-color", "white");
+        $(".cells").css({
+            "background-color": "white",
+            "opacity" : "1"
+        });
     });
     
     
 
-    function addCss ()
+    function addCss()
     {
         $(".cells").css({
             "width" : cellDimension,
@@ -26,24 +41,53 @@ $(document).ready(() => {
 
     }
 
-    function onHover ()
+    function onHoverNormal()
     {
+        ClearAndPrompt();
+        $(".cells").mouseenter(function () {
+            $(this).css("background-color", "red");
+        });
+    }
+
+    function onHoverRandom ()
+    {
+        ClearAndPrompt();
         $(".cells").mouseenter(function () {
             $(this).css("background-color", colourPallete[Math.floor(Math.random() * colourPallete.length)]);
+        });
+    }
+
+    function onHoverTrail()
+    {
+        ClearAndPrompt();
+        $(".cells").hover(function(){
+            $(this).css("background-color", "black");
+        }, function(){
+            $(this).animate({"background-color" : "white"}, 500);
+        });
+    }
+
+    function onHoverOpacity()
+    {
+        ClearAndPrompt();
+        $(".cells").mouseenter(function () {
+            $(this).css({
+                "background-color": "black",
+                "opacity" : (opacity = opacity + 0.1 ) % max
+            });
         });
     }
 
     function ClearAndPrompt()
     {
         $(".cells").remove();
-        gridWidth = prompt("How many cells wide", 5);
+        gridWidth = prompt("How many cells wide", 10);
         cellDimension = (600/gridWidth) - 2; // -2 for the 1px border on each side
         for ( var i = gridWidth*gridWidth; i != 0; i--)
         {
             $gridContainer.append($("<div>", {"class": "cells"}));
         }
         addCss();
-        onHover();
     }
 
 
